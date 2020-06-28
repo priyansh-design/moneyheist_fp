ebed3087725290344e7e2039cacc6e053e8ff6ef
priyansh-design <mehtapriyansh2000@gmail.com>
2020-06-27 23:34:01 -0500

    added character section

---
+++ js.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 15,
+    stretch: 0,
+    depth: 200,
+    modifier: 1,
+    slideShadows: true,
+  },
+  loop:true,
+  
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+    dynamicBullets:true,
+  },
+  
+});
