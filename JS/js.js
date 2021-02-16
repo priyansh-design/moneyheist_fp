f077588c6f1babf8c03081a3522e96261e07f3ed
priyansh-design <mehtapriyansh2000@gmail.com>
2021-02-16 22:57:15 +0530

    modified files

--- JS/js.js
+++ JS/js.js
@@ -18,3 +18,47 @@ var swiper = new Swiper(".swiper-container", {
     dynamicBullets: true,
   },
 });
+
+
+
+var blur=document.getElementById('blur');
+var tokyo=document.getElementById('tokyo');
+var nairobi=document.getElementById('nairobi');
+var berlin=document.getElementById('berlin');
+var bodyblur=document.getElementById('bodyblur')
+
+
+function t(){
+  blur.classList.toggle('active');
+  tokyo.classList.toggle('active');
+  bodyblur.classList.toggle('active');
+}
+
+function n(){
+  blur.classList.toggle('active')
+  nairobi.classList.toggle('active')
+}
+
+function b(){
+  blur.classList.toggle('active')
+  berlin.classList.toggle('active')
+}
+
+function tc(){
+  blur.classList.toggle('active')
+  tokyo.classList.toggle('active')
+  bodyblur.classList.toggle('active')
+
+}
+
+function nc(){
+  blur.classList.toggle('active')
+  nairobi.classList.toggle('active')
+  
+}
+
+function bec(){
+  blur.classList.toggle('active')
+  berlin.classList.toggle('active')
+  
+}
